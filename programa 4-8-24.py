338f40b25828a3466651f3c102dc46c071ffefc2
GodotJ <159208842+GodotJ@users.noreply.github.com>
2024-04-09 18:33:00 -0700

    Update programa 4-8-24.py

--- programa 4-8-24.py
+++ programa 4-8-24.py
@@ -1,9 +1,13 @@
-from colorama import Fore, Style
-
-
+# Pedir datos
 direccion_correo = input("Por favor ingresa tu dirección de correo: ")
 nombre = input("Por favor ingresa tu nombre: ")
 edad = input("Por favor ingresa tu edad: ")
 
+# Definir códigos de color
+color_verde = "\033[32m"
+color_blanco = "\033[37m"
+color_rojo = "\033[31m"
+reset_color = "\033[0m"
 
-print("Hola, mi nombre es", Fore.YELLOW + nombre + ",", "mi dirección es", Fore.MAGENTA + direccion_correo + ",", "y mi edad es", Fore.BLUE + edad + ".", Style.RESET_ALL)
+# Imprimir
+print("Hola, mi nombre es " + reset_color + color_verde + nombre + reset_color + ", mi dirección es " + reset_color + color_blanco + direccion_correo + reset_color + ", y mi edad es " + reset_color + color_rojo + edad + reset_color + ".")
